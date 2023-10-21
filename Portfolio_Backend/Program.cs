f379a130d56cac19097c2f563285b9c767b2a60d
Kevin <kevinlee.3191@gmail.com>
2023-10-21 12:19:54 -0700

    Changed url for frontend and backend

--- Portfolio_Backend/Program.cs
+++ Portfolio_Backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+                            policy.WithOrigins("https://kevinlee.app/").AllowAnyHeader().AllowAnyMethod();
                         });
 });
 
